efe336acc075236c1683bf0085bb9ad41c231078
Siriyakorn Dee-udomvongsa <Siriyakorn@Sopons-MacBook-Pro.local>
2025-05-19 12:45:02 +0700

    Creat color flie

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color tamarindBrown = Color(0xFF6D4C41);
+  static const Color ricePaddyGreen = Color(0xFF8BC34A);
+  static const Color chilliRed = Color(0xFFD84315);
+  
+  static const Color jasmineBeige = Color(0xFFFFF8E1);
+  static const Color bambooCream = Color(0xFFF5F5DC);
+}
