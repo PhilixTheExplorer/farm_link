21d9eee1963d88e000bfdac62f07d2a6bbdb2295
Siriyakorn <Siriyakorn.dee@gmail.com>
2025-05-19 13:53:01 +0700

    added farmer-dashview

---
+++ lib/screens/farmer_dashboard_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FarmerDashboardView extends StatefulWidget {
+  const FarmerDashboardView({super.key});
+
+  @override
+  State<FarmerDashboardView> createState() => _FarmerDashboardViewState();
+}
+
+class _FarmerDashboardViewState extends State<FarmerDashboardView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
